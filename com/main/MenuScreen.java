3a08a22d0668fd1c0f3e4ecfd0c1f6dbaef7d89f
mtorrejos <migueladrian.torrejos.cics@ust.edu.ph>
2023-12-01 18:56:33 +0800

    init commit, menu + sjf

---
+++ com/main/MenuScreen.java
@@ -0,0 +1,33 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class MenuScreen {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("1 - SCAN");
+		System.out.println("2 - Priority Scheduling");
+		System.out.println("3 - Shortest Job Next");
+		System.out.println("Choose a scheduling algorithm: ");
+		int choice = scanner.nextInt();
+
+		switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				SJF sjf = new SJF();
+				sjf.run();
+				break;
+				default:
+					System.out.print("Error: Invalid input");
+					break;
+			}
+		
+		
+
+	}
+}
---
+++ com/main/SJF.java
@@ -0,0 +1,84 @@
+package com.main;
+
+import java.util.*;
+
+public class SJF {
+
+    void run(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of processes:");
+        int n = in.nextInt();
+        ArrayList<Process> processes = new ArrayList<>();
+        int tot = 0;
+        float avgwt = 0, avgta = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter process " + (i + 1) + " arrival time: ");
+            int arrivalTime = in.nextInt();
+            System.out.print("Enter process " + (i + 1) + " burst time: ");
+            int burstTime = in.nextInt();
+            processes.add(new Process(i + 1, arrivalTime, burstTime));
+        }
+
+        int currentTime = 0;
+
+        while (true) {
+            if (tot == n) { 
+                break;
+            }
+
+            Process currentProcess = null;
+            int minBurstTime = Integer.MAX_VALUE;
+
+            for (Process process : processes) { //finds the min burst time repeatedly for non-complete processes
+                if (!process.completed && process.arrivalTime <= currentTime && process.burstTime < minBurstTime) {
+                    minBurstTime = process.burstTime;
+                    currentProcess = process;
+                }
+            }
+
+            if (currentProcess == null) {
+                currentTime++;
+            } else {
+                currentProcess.completionTime = currentTime + currentProcess.burstTime;
+                currentProcess.turnAroundTime = currentProcess.completionTime - currentProcess.arrivalTime;
+                currentProcess.waitingTime = currentProcess.turnAroundTime - currentProcess.burstTime;
+                currentProcess.completed = true;
+                currentTime += currentProcess.burstTime;
+                tot++;
+            }
+        }
+
+        System.out.println("\npID \tArrival \tBurst \tComplete \tTAT \tWaiting");
+        for (Process process : processes) {
+            avgwt += process.waitingTime;
+            avgta += process.turnAroundTime;
+            System.out.println(process.processId + "\t" + process.arrivalTime + "\t\t" + process.burstTime + "\t" +
+                    process.completionTime + "\t\t" + process.turnAroundTime + "\t" + process.waitingTime);
+        }
+
+        System.out.println("\naverage turnaround time is " + (float) (avgta / n));
+        System.out.println("average waiting time is " + (float) (avgwt / n));
+
+        in.close();
+    }
+}
+
+class Process {
+
+    int processId;
+    int arrivalTime;
+    int burstTime;
+    int completionTime;
+    int turnAroundTime;
+    int waitingTime;
+    boolean completed;
+
+    public Process(int processId, int arrivalTime, int burstTime) {
+        this.processId = processId;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.completed = false;
+    }
+}
